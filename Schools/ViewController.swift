a0545458a894b9e11d52ee5a17e29af168ca3ecb
Yongfeng <bravetsia@gmail.com>
2023-02-22 11:52:30 -0600

    Initial Commit

---
+++ Schools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schools
+//
+//  Created by 柴勇峰 on 2/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SchoolsUITests/SchoolsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SchoolsUITestsLaunchTests.swift
+//  SchoolsUITests
+//
+//  Created by 柴勇峰 on 2/22/23.
+//
+
+import XCTest
+
+final class SchoolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
